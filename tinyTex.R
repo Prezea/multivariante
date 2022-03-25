0954ae24cb66fc935c3adb0c79962cded3d2ada3
Yendi Lestrade <65693440+Prezea@users.noreply.github.com>
2022-03-25 11:53:48 -0600

    TinyTeX

---
+++ tinyTex.R
@@ -0,0 +1,12 @@
+
+#-------------------------------
+#   TinyTex para Markdown
+#-------------------------------
+
+
+# instalacion de TinyTeX
+install.packages('tinytex')
+tinytex::install_tinytex()
+
+# comprobar la instalación
+tinytex:::is_tinytex()
